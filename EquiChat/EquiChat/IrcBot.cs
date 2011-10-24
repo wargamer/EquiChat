004d7fb20bb59b2a5f9cbfea1cae91119b94fc77
Vincent Spaa <vincentspaa@hotmail.com>
2011-10-25 00:19:43 +0200

    Added rough support for "Username in use" and removed unneeded code.

--- EquiChat/EquiChat/IrcBot.cs
+++ EquiChat/EquiChat/IrcBot.cs
@@ -16,222 +16,198 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-class IrcBot : DispatcherObject
-{   
-    private static int PORT = 6667;    
-    private Thread bot;
-    
-    private string nick;
-    private string channel;
-    private string user;
-    private string hostname;
-    private TextBlock chat;
-    private PingSender ping;
-    static bool started = false;
-
-    private NetworkStream stream;
-    private TcpClient irc;
-    private StreamReader reader;
-
-    // StreamWriter is declared here so that PingSender can access it
-    public static StreamWriter writer;
-
-    public IrcBot () {        
-        bot = new Thread(new ThreadStart(this.Run));
-	}
-
-    public void Start(string pNick, string pChannel, string pUser, string pHostname, TextBlock pChat)
-    {
-        if (pNick == "" || pChannel == "" || pUser == "" || pHostname == "" || started == true)
-            return;
-        started = true;
-        nick = pNick; channel = pChannel; user = pUser; hostname = pHostname;
-        chat = pChat;
-
-        bot.Start();        
-    }
-
-    public void Stop()
-    {        
-        bot.Abort();
-    }
-
-    private object InvokeMethod(Delegate method, params object[] args)
-    {
-        return method.DynamicInvoke(args);
-    }
-
-    public void sendMessage(string line)
-    {
-        writer.WriteLine("PRIVMSG " + channel + " " + line);
-        writer.Flush();
-        addLineToChat("<" + nick + "> " + line);
-    }
-
-    private string[] parseChatMessage(string rawMessage)
-    {
-        Match match = Regex.Match(rawMessage, @"^\:(.+)!.+@.+\sPRIVMSG\s\#[A-Z]+\s\:(.+)$", RegexOptions.IgnoreCase);         
-        if (match.Success)
-        {
-            // 1 = user, 2 = message
-            string[] answer = { match.Groups[1].Value, match.Groups[2].Value };
-            return answer;
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    private string[] parseAnnouncement(string rawMessage)
-    {
-        Match match = Regex.Match(rawMessage, @"^\:[A-Z0-9.-]*\.[A-Z0-9.-]+\.[A-Z]{2,4}\s([0-9]+)\s[^\s\t]+\s\:(.+)$", RegexOptions.IgnoreCase);
-        if (match.Success)
-        {
-            // 1 = messagecode, 2 = message
-            string[] answer = { match.Groups[1].Value, match.Groups[2].Value };
-            return answer;
-        }
-        else
-        {
-            return null;
-        }
-    }
-
-    private void addLineToChat(string line)
-    {
-        Action<String> writeLine;
-        writeLine = delegate(string s)
-        {
-            chat.Text = s + "\r\n" + chat.Text;
-        };
-        chat.Dispatcher.BeginInvoke(DispatcherPriority.Normal, writeLine, line);
-    }
-
-    private void stopNow()
-    {
-        Console.WriteLine("!!IrcBot caught an abort exception, stopping NOW!!");
-        ping.Stop();
-        writer.Close();
-        reader.Close();
-        irc.Close();        
-    }
-
-    private void sleepWithBreaks(int sleepTime)
-    {
-        int sleptTime = 0;
-        while (sleptTime < sleepTime)
-        {            
-            Thread.Sleep(10);
-            sleptTime += 10;            
-        }        
-    }
-
-    private string ReadLineWithTimeout(int milliseconds)
+namespace EquiChat {
+    class IrcBot : DispatcherObject
     {   
-        AutoResetEvent autoEvent = new AutoResetEvent(false);
-        string buffer;
+        private static int PORT = 6667;    
+        private Thread bot;
+    
+        private string nick;
+        private string channel;
+        private string user;
+        private string hostname;
+        private TextBlock chat;
+        private PingSender ping;
+        static bool started = false;
 
-        System.Threading.Timer timeoutTimer = new Timer (
-            (Object stateInfo) =>
-            {
-                Console.WriteLine("TIMEOUT @ ReadLineWithTimeout !");
-            },
-            autoEvent,
-            milliseconds,
-            0);
-        buffer = reader.ReadLine();
+        private NetworkStream stream;
+        private TcpClient irc;
+        private StreamReader reader;
 
-        return buffer;
+        // StreamWriter is declared here so that PingSender can access it
+        public static StreamWriter writer;
 
-    }
+        public IrcBot () {        
+            bot = new Thread(new ThreadStart(this.Run));
+	    }
 
-    public void Run()
-    {
-        string inputLine = "";        
-
-        try
+        public void Start(string pNick, string pChannel, string pUser, string pHostname, TextBlock pChat)
         {
-            if (connect())
+            if (pNick == "" || pChannel == "" || pUser == "" || pHostname == "" || started == true)
+                return;
+            started = true;
+            nick = pNick; channel = pChannel; user = pUser; hostname = pHostname;
+            chat = pChat;
+
+            bot.Name = ("IRC BOT " + hostname);
+            bot.Start();
+        }
+
+        public void Stop()
+        {        
+            bot.Abort();
+        }
+    
+        public void sendMessage(string line)
+        {
+            writer.WriteLine("PRIVMSG " + channel + " " + line);
+            writer.Flush();
+            addLineToChat("<" + nick + "> " + line);
+        }
+
+        private string[] parseChatMessage(string rawMessage)
+        {
+            Match match = Regex.Match(rawMessage, @Constants.privmsgRegexp, RegexOptions.IgnoreCase);         
+            if (match.Success)
             {
-                ping = new PingSender(this);
-                ping.Start();
-                writer.WriteLine("USER " + user);
-                writer.Flush();
-                writer.WriteLine("NICK " + nick);
-                writer.Flush();
-                writer.WriteLine("JOIN " + channel);
-                writer.Flush();                                
-                while (true)
-                {
-                    DateTime startTime = DateTime.Now;
-                    TimeSpan timeout = TimeSpan.FromMilliseconds(10);
-                    inputLine = "";
-                    while (DateTime.Now.Subtract(startTime) < timeout && inputLine == "")
-                    {
-                        if (reader.Peek() > -1)
-                        {                            
-                            inputLine = reader.ReadLine();
-                        }                        
-                    }
-                    //if ((inputLine = ReadLineWithTimeout(10)) != "") //reader.ReadLine()
-                    if(inputLine != "")
-                    {
-                        string[] message;                        
-                        if (inputLine.Contains("PRIVMSG") && (message = parseChatMessage(inputLine)) != null)
-                        {
-                            string temp = "<" + message[0] + "> " + message[1];
-                            addLineToChat(temp);
-                        }
-                        else if ((message = parseAnnouncement(inputLine)) != null)
-                        {
-                            if (message[0] == "001")
-                            {
-                                addLineToChat(message[1]);
-                                addLineToChat("\n");
-                            }
-                        }
-                        Console.WriteLine("!!DEBUG: " + inputLine);                        
-                    }                    
-                }
+                // 1 = user, 2 = message
+                string[] answer = { match.Groups[1].Value, match.Groups[2].Value };
+                return answer;
             }
             else
             {
-                Console.WriteLine("Connection failed.");
+                return null;
+            }
+        }
+
+        private string[] parseAnnouncement(string rawMessage)
+        {
+            Match match = Regex.Match(rawMessage, @Constants.announcementRegexp, RegexOptions.IgnoreCase);
+            if (match.Success)
+            {
+                // 1 = messagecode, 2 = message
+                string[] answer = { match.Groups[1].Value, match.Groups[2].Value };
+                return answer;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void addLineToChat(string line)
+        {
+            Action<String> writeLine;
+            writeLine = delegate(string s)
+            {
+                chat.Text = chat.Text + "\r\n" + s;
+            };
+            chat.Dispatcher.BeginInvoke(DispatcherPriority.Normal, writeLine, line);
+        }
+
+        private void stopNow()
+        {
+            Console.WriteLine("!!IrcBot caught an abort exception, stopping NOW!!");
+            ping.Stop();
+            writer.Close();
+            reader.Close();
+            irc.Close();        
+        }
+
+        private void sleepWithBreaks(int sleepTime)
+        {
+            int sleptTime = 0;
+            while (sleptTime < sleepTime)
+            {            
+                Thread.Sleep(10);
+                sleptTime += 10;            
+            }        
+        }
+
+        public void Run()
+        {
+            string inputLine = "";        
+
+            try
+            {
+                if (connect())
+                {
+                    ping = new PingSender(this);
+                    ping.Start();
+                    writer.WriteLine("USER " + user);
+                    writer.Flush();
+                    writer.WriteLine("NICK " + nick);
+                    writer.Flush();
+                    writer.WriteLine("JOIN " + channel);
+                    writer.Flush();                                
+                    while (true)
+                    {                    
+                        inputLine = "";
+                        if ((inputLine = reader.ReadLine()) != "")                            
+                        {
+                            string[] message;
+                            if (inputLine.Contains("PRIVMSG") && (message = parseChatMessage(inputLine)) != null)
+                            {
+                                string temp = "<" + message[0] + "> " + message[1];
+                                addLineToChat(temp);
+                                writer.WriteLine("NICK " + message[1]);
+                                writer.Flush();
+                            }
+                            else if ((message = parseAnnouncement(inputLine)) != null)
+                            {
+                                if (message[0] == "001")
+                                {
+                                    addLineToChat(message[1]);
+                                    addLineToChat("\n");
+                                }
+                                else if (message[0] == "433")
+                                {
+                                    addLineToChat("Username is already in use!");
+                                }
+                            } 
+                            Console.WriteLine("DEBUG: " + inputLine);
+                        }
+                    
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Connection failed.");
+                    sleepWithBreaks(5000);
+                    Run();
+                }
+            }
+            catch (ThreadAbortException abortException)
+            {
+                stopNow();
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
                 sleepWithBreaks(5000);
                 Run();
             }
         }
-        catch (ThreadAbortException abortException)
-        {
-            stopNow();
-            return;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.ToString());
-            sleepWithBreaks(5000);
-            Run();
-        }
-    }
 
-    public string getHostname()
-    {
-        return hostname;
-    }
-
-    private bool connect()
-    {
-        try
+        public string getHostname()
         {
-            irc = new TcpClient(hostname, PORT);
-            stream = irc.GetStream();
-            reader = new StreamReader(stream);
-            writer = new StreamWriter(stream);
-            return true;
-        } catch(Exception e) {
-            Console.WriteLine(e.ToString());
-            return false;
+            return hostname;
+        }
+
+        private bool connect()
+        {
+            try
+            {
+                irc = new TcpClient(hostname, PORT);
+                stream = irc.GetStream();
+                reader = new StreamReader(stream);
+                writer = new StreamWriter(stream);
+                return true;
+            } catch(Exception e) {
+                Console.WriteLine(e.ToString());
+                return false;
+            }
         }
     }
 }
--- EquiChat/EquiChat/PingSender.cs
+++ EquiChat/EquiChat/PingSender.cs
@@ -1,58 +1,62 @@
 ﻿using System;
 using System.Threading;
 
-/*
-* Class that sends PING to irc server every 15 seconds
-*/
-class PingSender {
-	static string PING = "PING :";
-	private Thread pingSender;
-    private IrcBot bot;
-    private int sleepTime = 15000;
-	
-	// Empty constructor makes instance of Thread
-	public PingSender (IrcBot pBot) {
-		pingSender = new Thread (new ThreadStart (this.Run) );
-        bot = pBot;
-	}
-	
-	// Starts the thread
-	public void Start () {
-		pingSender.Start ();
-	}
-
-    public void Stop()
+namespace EquiChat
+{
+    class PingSender
     {
-        
-        pingSender.Abort();
-    }
+        static string PING = "PING :";
+        private Thread pingSender;
+        private IrcBot bot;
+        private int sleepTime = 15000;
 
-    private void sleepWithBreaks(int sleepTime)
-    {
-        int sleptTime = 0;
-        while (sleptTime < sleepTime)
+        // Empty constructor makes instance of Thread
+        public PingSender(IrcBot pBot)
         {
-            Thread.Sleep(10);
-            sleptTime += 10;
+            pingSender = new Thread(new ThreadStart(this.Run));
+            bot = pBot;
+        }
+
+        // Starts the thread
+        public void Start()
+        {
+            pingSender.Start();
+        }
+
+        public void Stop()
+        {
+
+            pingSender.Abort();
+        }
+
+        private void sleepWithBreaks(int sleepTime)
+        {
+            int sleptTime = 0;
+            while (sleptTime < sleepTime)
+            {
+                Thread.Sleep(10);
+                sleptTime += 10;
+            }
+        }
+
+        // Send PING to irc server every 15 seconds
+        public void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    IrcBot.writer.WriteLine(PING + bot.getHostname());
+                    IrcBot.writer.Flush();
+                    sleepWithBreaks(sleepTime);
+                }
+                catch (ThreadAbortException abortException)
+                {
+                    Console.WriteLine("!!PingSender caught an abort exception, stopping NOW!!");
+                    Console.WriteLine((string)abortException.ExceptionState);
+                    return;
+                }
+            }
         }
     }
-	
-	// Send PING to irc server every 15 seconds
-	public void Run () {        
-		while (true)
-		{
-            try
-            {
-                IrcBot.writer.WriteLine(PING + bot.getHostname());
-                IrcBot.writer.Flush();
-                sleepWithBreaks(sleepTime);
-            }
-            catch (ThreadAbortException abortException)
-            {
-                Console.WriteLine("!!PingSender caught an abort exception, stopping NOW!!");
-                Console.WriteLine((string)abortException.ExceptionState);
-                return;
-            }
-		}
-	}
 }
