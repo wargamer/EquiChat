4be13b58e3ce168871ce1cf08fd6c7ed5f2ce634
Vincent Spaa <vincentspaa@hotmail.com>
2011-10-27 20:46:05 +0200

    Added missing about files

---
+++ EquiChat/EquiChat/About.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EquiChat
+{
+    /// <summary>
+    /// Interaction logic for About.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            text.Text = "\t\tEquichat\n\nA simple chat client that uses IRC to communicate.\nIt shows the players that are online and which game they are playing.\nAnd it is open source.\n\n" +
+            "Authors:\n" +
+            "Raymond Meerburg\n" +
+            "Hugo Meijer\n" +
+            "Vincent Spaa\n";
+        }
+    }
+}
